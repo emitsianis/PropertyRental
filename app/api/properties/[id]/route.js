d639e0d35f447b51329b7a85ab4fefa9d1838e14
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-04-01 20:57:11 +0300

    Add: Delete properties/toast

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,5 +1,6 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import { getSessionUser } from '@/utils/getSessionUser';
 
 // GET /api/properties/:id
 export const GET = async (request, { params }) => {
@@ -10,9 +11,39 @@ export const GET = async (request, { params }) => {
 
     if (!property) return new Response('Property not found', { status: 404 });
 
-    return new Response(JSON.stringify(property), { status: 200 })
+    return new Response(JSON.stringify(property), { status: 200 });
   } catch (e) {
     console.log(e);
-    return new Response('Something went wrong', { status: 500 })
+    return new Response('Something went wrong', { status: 500 });
   }
-}
+};
+
+// DELETE /api/properties/:id
+export const DELETE = async (request, { params }) => {
+  try {
+    const propertyId = params.id;
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser?.userId) return new Response('User id is required', { status: 401 });
+
+    const { userId } = sessionUser;
+
+    await connectDB();
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) return new Response('Property not found', { status: 404 });
+
+    if (property.owner.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    await property.deleteOne();
+
+    return new Response('Property deleted.', { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
--- app/layout.jsx
+++ app/layout.jsx
@@ -2,12 +2,14 @@ import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import AuthProvider from '@/components/AuthProvider';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 export const metadata = {
   title: 'PropertyPulse | Find the perfect rental',
   description: 'Find your dream rental property',
   keywords: 'rental, find rentals, find properties',
-}
+};
 
 const MainLayout = ({ children }) => {
   return (
@@ -19,6 +21,7 @@ const MainLayout = ({ children }) => {
             {children}
           </main>
           <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
