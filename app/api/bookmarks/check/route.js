338ba26d9cbf54f35291c5955195c68e3d0f9567
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-04-05 22:01:31 +0300

    Add: Bookmarking properties

--- app/api/bookmarks/check/route.js
+++ app/api/bookmarks/check/route.js
@@ -16,21 +16,8 @@ export const POST = async (request) => {
     const user = await User.findById(userId);
 
     let isBookmarked = user?.bookmarks?.includes(propertyId);
-    let message;
 
-    if (isBookmarked) {
-      user?.bookmarks?.pull(propertyId);
-      message = 'Property removed from bookmarks';
-      isBookmarked = false;
-    } else {
-      user?.bookmarks?.push(propertyId);
-      message = 'Property added to bookmarks';
-      isBookmarked = true;
-    }
-
-    await user?.save();
-
-    return new Response(JSON.stringify({ message, isBookmarked }), { status: 200 });
+    return new Response(JSON.stringify({ isBookmarked }), { status: 200 });
   } catch (e) {
     console.log(e);
     return new Response('Internal Server Error', { status: 500 });
--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -1,6 +1,6 @@
 import { FaShare } from 'react-icons/fa';
 
-const MyComponent = () => {
+const ShareButton = () => {
   return (
     <button
       className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
@@ -10,4 +10,4 @@ const MyComponent = () => {
   );
 };
 
-export default MyComponent;
+export default ShareButton;
