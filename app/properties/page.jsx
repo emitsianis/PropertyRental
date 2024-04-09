03245a7e760d31b1289324a2fd1fcd13ca33a3a6
emitsianis <ev.mitsianis@gmail.com>
2024-04-09 19:45:49 +0300

    Add: Search properties functionality

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,20 @@
 import PropertyCard from '@/components/PropertyCard';
 import { fetchProperties } from '@/utils/requests';
+import PropertySearchForm from '@/components/PropertySearchForm';
+import Spinner from '@/components/Spinner';
+import Link from 'next/link';
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
 
-  return (
+  return <>
+    <section className="bg-blue-700 py-4">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+        <PropertySearchForm
+        />
+      </div>
+    </section>
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
         {properties.length === 0 ? (
@@ -18,7 +28,7 @@ const PropertiesPage = async () => {
         )}
       </div>
     </section>
-  );
+  </>;
 };
 
 export default PropertiesPage;
