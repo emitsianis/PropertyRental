263c2acfc3c1dd049cc97246016b7ef9f69c801c
emitsianis <ev.mitsianis@gmail.com>
2024-04-12 20:35:16 +0300

    Add: Properties pagination

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -8,11 +8,18 @@ export const GET = async (request) => {
   try {
     await connectDB();
 
-    const properties = await Property.find({});
+    const page = request.nextUrl.searchParams.get('page') || 1;
+    const pageSize = request.nextUrl.searchParams.get('pageSize') || 3;
+
+    const skip = (page - 1) * pageSize;
+
+    const total = await Property.countDocuments({});
+
+    const properties = await Property.find({}).skip(skip).limit(pageSize);
 
     properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
-    return new Response(JSON.stringify(properties), { status: 200 });
+    return new Response(JSON.stringify({ total, properties }), { status: 200 });
   } catch (e) {
     console.log(e);
     return new Response('Something went wrong', { status: 500 });
@@ -75,7 +82,7 @@ export const POST = async (request) => {
         `data:image/png;base64,${imageBase64}`,
         {
           folder: 'PropertyRental',
-        }
+        },
       );
 
       imageUploadPromises.push(result.secure_url);
